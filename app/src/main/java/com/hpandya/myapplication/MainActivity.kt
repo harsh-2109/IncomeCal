60271cf1635e0d43afd6d9d8d42d70a4859dcd3e
harsh-2109 <harshdpandya1995@gmail.com>
2021-10-08 11:21:04 -0400

    This project will calculate deducted amounts from gross salaries and provide information on net revenue and deducted amounts.

---
+++ app/src/main/java/com/hpandya/myapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.hpandya.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var grossAnnualIncome:TextInputEditText
+    lateinit var totalTaxAmount:TextView
+    lateinit var federalTax:TextView
+    lateinit var provincialTax:TextView
+    lateinit var cppEiPremiums:TextView
+    lateinit var afterTaxIncome:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Initialise the value
+        grossAnnualIncome= findViewById(R.id.gross_amount)
+        totalTaxAmount=findViewById(R.id.total_tax_amount_text_view)
+        federalTax=findViewById(R.id.federal_tax_amount_text_view)
+        provincialTax=findViewById(R.id.provincial_tax_amount_text_view)
+        cppEiPremiums=findViewById(R.id.cpp_ei_premiums_amount_text_view)
+        afterTaxIncome=findViewById(R.id.after_tax_income_amount_text_view)
+
+        grossAnnualIncome.addTextChangedListener{
+            var grossIncome:String=grossAnnualIncome.text.toString()
+            if (grossIncome == ""){
+                grossIncome="0"
+            }
+            val totalAmount=Integer.valueOf(grossIncome)
+            if (totalAmount<49020) {
+                val totalTax = totalAmount * 0.1969
+                totalTaxAmount.text = (totalTax).toString()
+                federalTax.text = (totalAmount * 0.0863).toString()
+                provincialTax.text = (totalAmount * 0.0448).toString()
+                cppEiPremiums.text = (totalAmount * 0.0657).toString()
+                afterTaxIncome.text = (totalAmount - totalTax).toString()
+            }else if (totalAmount>=49020 == totalAmount<98040){
+                val totalTax = totalAmount * 0.1969
+                totalTaxAmount.text = (totalTax).toString()
+                federalTax.text = (totalAmount * 0.0863).toString()
+                provincialTax.text = (totalAmount * 0.0448).toString()
+                cppEiPremiums.text = (totalAmount * 0.0657).toString()
+                afterTaxIncome.text = (totalAmount - totalTax).toString()
+            }
+        }
+    }
+
+}
